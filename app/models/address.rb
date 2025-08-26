88a64065ffda12b1f838b7022faa6558b1a7d929
Gael Roussel <gael.roussel@mac.com>
2025-08-26 11:03:50 +0200

    final_models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
-  has_one :itinerary_objectives, dependent: :destroy
+  has_one :departure_itinerary_objectives,
+           class_name: "ItineraryObjective",
+           foreign_key: "departure_address_id",
+           dependent: :nullify
+
+  has_one :arrival_itinerary_objectives,
+           class_name: "ItineraryObjective",
+           foreign_key: "arrival_address_id",
+           dependent: :nullify
+
+  has_many :point_of_interests, dependent: :destroy
 end
