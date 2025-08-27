8717b8798a5adaf21d89684a399a224edb29c5c1
Baptiste Leblanc <baptisteleblanc8@gmail.com>
2025-08-27 12:00:17 +0200

    commit before pull

--- app/controllers/itinerary_objectives_controller.rb
+++ app/controllers/itinerary_objectives_controller.rb
@@ -1,4 +1,13 @@
 class ItineraryObjectivesController < ApplicationController
+  def create
+    @itinerary_objective = ItineraryObjective.new(itinerary_objective_params)
+    if @itinerary_objective.save
+      redirect_to root_path, notice: "DOne"
+    else
+      redirect_to root_path, alert: "Error"
+    end
+  end
+
   def edit
     @address = address.find(address_params)
     @address.save
@@ -12,5 +21,9 @@ class ItineraryObjectivesController < ApplicationController
 
   def address_params
     params.require(:address).permit(:longitude, :latitude)
+  end
+
+  def itinerary_objective_params
+    params.require(:itinerary_objective).permit(:address)
   end
 end
