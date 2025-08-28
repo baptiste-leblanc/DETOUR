0450163776cc769639ca55180e674cc46a8db6ab
Gael Roussel <gael.roussel@mac.com>
2025-08-28 16:19:37 +0200

    Wip

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="address-autocomplete"
@@ -9,6 +8,8 @@ export default class extends Controller {
   static targets = ["address"]
   connect() {
     this.geocoder = new MapboxGeocoder({
+      enableGeolocation: this.addressTarget.dataset.enableGeolocation === "true",
+      placeholder: this.addressTarget.placeholder || "Search",
       accessToken: this.apiKeyValue,
       types: "country,region,place,locality,neighborhood,address",
       language: 'fr'
@@ -17,6 +18,9 @@ export default class extends Controller {
     this.geocoder.addTo(this.element)
     this.geocoder.on("result", event => this.#setInputValue(event))
     this.geocoder.on("clear", () => this.#clearInputValue())
+
+
+    this.element.querySelector("button.mapboxgl-ctrl-geocoder--button[aria-label=Geolocate]")?.setAttribute("type", "button")
   }
 
   disconnect() {
